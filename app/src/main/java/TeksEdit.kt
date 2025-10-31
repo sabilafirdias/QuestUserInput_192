42cd94d0f8ebc2c092c6f90a7781114a965507b4
sabilafirdias <sabilafirdias@gmail.com>
2025-10-31 09:12:57 +0700

    menambahkan func FormDataDiri

---
+++ app/src/main/java/TeksEdit.kt
@@ -0,0 +1,10 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier)
+{
+
+}
