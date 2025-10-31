d91e39233b92af468741dc430271cd42a7b532ce
sabilafirdias <sabilafirdias@gmail.com>
2025-10-31 09:26:42 +0700

    membuat text field

--- app/src/main/java/TeksEdit.kt
+++ app/src/main/java/TeksEdit.kt
@@ -36,7 +36,16 @@ fun FormDataDiri(modifier: Modifier)
     verticalArrangement = Arrangement.Top,
 
         horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
 
+            label = { Text(text = "Nama Lengkap") },
+
+            onValueChange = {
+                textNama = it
             }
         )
 
