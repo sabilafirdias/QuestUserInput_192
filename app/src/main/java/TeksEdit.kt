6be84664ebf18ae79f1e69b6fa985a0336c7b1d3
sabilafirdias <sabilafirdias@gmail.com>
2025-10-31 09:15:46 +0700

    menambahkan import by

--- app/src/main/java/TeksEdit.kt
+++ app/src/main/java/TeksEdit.kt
@@ -1,6 +1,8 @@
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
