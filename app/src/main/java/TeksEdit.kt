7bfac747d0bb0669fa389230c1e32651316938a7
sabilafirdias <sabilafirdias@gmail.com>
2025-10-31 09:38:54 +0700

    membuat radio button

--- app/src/main/java/TeksEdit.kt
+++ app/src/main/java/TeksEdit.kt
@@ -53,7 +53,19 @@ fun FormDataDiri(modifier: Modifier)
 
     Row {
         gender.forEach { item ->
+            Row(modifier = Modifier.selectable(
+                selected = textJK == item,
 
+                onClick = { textJK = item }
+            ), verticalAlignment = Alignment.CenterVertically) {
+                RadioButton(
+                    selected = textJK == item,
+
+                    onClick = {
+                        textJK = item
+                    })
+                Text(item)
+            }
         }
     }
 
