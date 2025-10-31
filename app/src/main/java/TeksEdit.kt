c67823fd8a95a0cecf7a62949ec909643b29fa27
sabilafirdias <sabilafirdias@gmail.com>
2025-10-31 09:38:33 +0700

    membuat row

--- app/src/main/java/TeksEdit.kt
+++ app/src/main/java/TeksEdit.kt
@@ -3,8 +3,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -50,7 +52,11 @@ fun FormDataDiri(modifier: Modifier)
         )
 
     Row {
+        gender.forEach { item ->
 
+        }
     }
+
+        )
 }
 }
