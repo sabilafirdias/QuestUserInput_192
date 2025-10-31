af98b36a7eca9f07fd3c6d603c6ff7cf06abf690
sabilafirdias <sabilafirdias@gmail.com>
2025-10-31 09:54:46 +0700

    membuat divider

--- app/src/main/java/TeksEdit.kt
+++ app/src/main/java/TeksEdit.kt
@@ -87,6 +87,13 @@ fun FormDataDiri(modifier: Modifier)
         }
     )
 
+    Divider(
+        modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+            id = R.dimen.padding_medium
+        )),
+        thickness = dimensionResource(R.dimen.divider_tipis),
+        color = Color.DarkGray
+    )
 
 }
 }
